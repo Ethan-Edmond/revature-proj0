dbfadbf7417248faa43dcff23e4faa8b1ecc023a
Ethan Edmond <eedmond@uccs.edu>
2022-02-16 09:28:54 -0700

    Initial commit

---
+++ src/main/java/com/ethanedmond/alarm/Driver.java
@@ -0,0 +1,14 @@
+package com.ethanedmond.alarm;
+
+public class Driver {
+    public static void main(String[] args) {
+        // TODO
+        // play a sound with prompt until user shows that they are awake
+        // prompt them to input a journal entry
+        // save journal entry into azure database
+        // print message that confirms to user that the entry has been correctly saved
+
+        // TODO figure out a version for a go to sleep alarm
+        // TODO get this to run with a batch file
+    }
+}
